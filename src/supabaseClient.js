f84709609c23c3b3b92905401e7f94d8c68114ea
Andre Gomez <drecoding101001@gmail.com>
2023-03-17 15:37:31 -0600

    added comment for env files

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Import from .env file use [process.env.NAME]
+// To import from .env using vite, use [import.meta.env.NAME]
+
 const supabaseURL = import.meta.env.VITE_REACT_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_REACT_SUPABASE_ANON_KEY
 
